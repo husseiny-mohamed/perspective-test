943c11d5fb4fe263fc7202c89fc6b1329f1eeb13
Husseiny <husseiny.mohammed@gmail.com>
2019-07-26 17:30:30 +0200

    added test component UI

--- src/app/perspective-test/perspective-test.module.ts
+++ src/app/perspective-test/perspective-test.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { QuestionComponent } from './question/question.component';
-import { QuestionListComponent } from './question-list/question-list.component';
-import { EmailComponent } from './email/email.component';
 import { ResultComponent } from './result/result.component';
 import { TestComponent } from './test/test.component';
 import { PerspectiveTestRoutingModule } from './perspective-test-routing.module';
+import { FormsModule } from '@angular/forms';
+
 
 
 
 @NgModule({
-  declarations: [QuestionComponent, QuestionListComponent, EmailComponent, ResultComponent, TestComponent],
+  declarations: [ResultComponent, TestComponent],
   imports: [
     CommonModule,
+    FormsModule,
     PerspectiveTestRoutingModule
   ]
 })
--- src/app/perspective-test/test/test.component.ts
+++ src/app/perspective-test/test/test.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
+  choicesArray: Array<number> = [1, 2, 3, 4, 5, 6, 7];
 
-  constructor() { }
+  questions = [
+    {
+      id: '1',
+      text: 'You find it takes effort to introduce yourself to other people.',
+      answer: 0
+    },
+    {
+      id: '2',
+      text: 'You consider yourself more practical than creative.',
+      answer: 0
+    },
+    {
+      id: '3',
+      text: 'Winning a debate matters less to you than making sure no one gets upset.',
+      answer: 0
+    },
+    {
+      id: '4',
+      text: 'You get energized going to social events that involve many interactions.',
+      answer: 0
+    },
+    {
+      id: '5',
+      text: 'You often spend time exploring unrealistic and impractical yet intriguing ideas.',
+      answer: 0
+    }
+  ];
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
+
+  save() {}
 }
